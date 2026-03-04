7f254fee9ee0aee11ce37687576f850f275d8571
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-03-04 14:33:58 +0600

    act

---
+++ module 1/lesson 3/activity2.py
@@ -0,0 +1,9 @@
+#name = input('whats your name?')
+#print('Hello ,',name,'! welcome to python')
+
+name1 = 'Zulkar'
+print(name1.upper())
+print(name1.lower())
+print(name1[::-1])
+name2 = 'naeen'
+print(name1+name2)
