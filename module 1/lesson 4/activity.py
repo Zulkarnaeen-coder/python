4bcd5ce7e4d01576ce64aadf7f09aa85fd31e7cd
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-03-05 14:18:09 +0600

    act1

---
+++ module 1/lesson 4/activity.py
@@ -0,0 +1,11 @@
+tr1 = 98
+tr2 = 96
+tr3 = 46
+tr4 = 94
+tr5 = 11
+
+sum = tr1+tr2+tr3+tr4+tr5
+print("The total heigth of the trees is",sum)
+
+avg = sum//5
+print("The average height of the tree is",avg)
