c7b075e3c3f3a43fdf5ac554ecf5b39e8a667c45
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-03-10 00:28:57 +0600

    acp

--- module 1/LESSON 5/acp/activity.py
+++ module 1/LESSON 5/acp/activity.py
@@ -8,19 +8,24 @@ if temp > 35 :
 elif temp <= 34 and temp > 29:
     temp = round(temp,2)
     print("The weather is hot and the temp is ",temp,"and you should wear soft dress!")
+
 #For 29-25 deg.
 elif temp < 29 and temp > 25 :
     temp = round(temp,2)
     print("The weather is quite hot and the temp is ",temp,"and you should wear normal soft dress!")
+
 #For 24-20 deg.
 elif temp < 24 and temp > 20 :
     temp = round(temp,2)
     print("The weather is quite cold and the temp is ",temp,"and you should wear soft huddyies!")
+
 #For 20-0 deg.
 elif temp < 20 and temp > 0 :
     temp = round(temp,2)
     print("The weather is cold and the temp is ",temp,"and you should wear hot or normal hoddies!")
+
 #for below O deg.
 else :
     temp = round(temp,2)
     print("The weather is too cold and the temp is ",temp,"and you have to wear big and hot sweater and under the sweather you should wear hoddies!")
+    
