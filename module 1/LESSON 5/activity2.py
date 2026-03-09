4e7f9862cbcf9f42481e5a8ed14f777f56bc018a
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-03-09 14:30:00 +0600

    act

---
+++ module 1/LESSON 5/activity2.py
@@ -0,0 +1,9 @@
+buy_price = float(input("Enter a buying price >>>"))
+sell_price =float(input("Enter a selling price >>>"))
+
+if buy_price <sell_price:
+    amount = buy_price-sell_price
+    print("The profit of",amount)
+
+else:
+    print("Loss")
