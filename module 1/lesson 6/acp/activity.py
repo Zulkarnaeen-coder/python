302a8bed2f3e6f1192a7b295e79f34f955143ac7
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-03-11 23:14:10 +0600

    acp

---
+++ module 1/lesson 6/acp/activity.py
@@ -0,0 +1,10 @@
+letter = input("Please Input a charecter >>>>>")
+
+if letter >= "A" and letter <= "Z":
+    print("It is a Alphabet")
+
+elif letter >= "a" and letter <= "z":
+    print("It is a Alphabet")
+
+else :
+    print("It is not a alphabet")
