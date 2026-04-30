0e1c1fc8ceab424edf260d8298edf5640839fcdd
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-04-30 20:48:19 +0600

    activity

---
+++ module 3/lesson 6/activity.py
@@ -0,0 +1,10 @@
+from datetime import date,time,datetime
+
+day = date.today()
+
+now = datetime.now()
+
+print("Today's date:",day)
+print("Current time :",now)
+
+print("The current date is ==",day.year , day.month , day.day)
