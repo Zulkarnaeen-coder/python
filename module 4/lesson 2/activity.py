15c39a8e8aa770327ba7fb93034c531eac37a513
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-05-06 20:51:04 +0600

    act

---
+++ module 4/lesson 2/activity.py
@@ -0,0 +1,16 @@
+tp = ("python","js",True,1,2,3)
+print(tp)
+
+tp2 =(1,2,3,4,5)
+print(tp2)
+
+print(f"Length of tuple 1 is :{len(tp)}")
+
+print(tp[3])
+
+tp = tp+(9,)
+
+print(tp)
+
+
+print(tp2[2:5])
