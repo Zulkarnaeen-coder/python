b1fec1385dd8bcce98957fe8d6f384f9e10233c0
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-05-12 21:13:12 +0600

    act

---
+++ module 4/lesson 3/activity2.py
@@ -0,0 +1,10 @@
+d1 ={"Codingal": 2 , "Is" : 2 , "Best": 2 , "For" : 2 , "Coding" : 1}
+
+k = 2 
+result = 0
+
+for key in d1:
+    if d1[key] == k:
+        result = result + 1
+
+print(f"Frequency == {result}")
