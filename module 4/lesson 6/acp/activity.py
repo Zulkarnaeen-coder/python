4ddc430dd37b42656dcfc510023dcd154a499e38
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-05-21 22:42:31 +0600

    a

--- module 4/lesson 6/acp/activity.py
+++ module 4/lesson 6/acp/activity.py
@@ -1,40 +1,41 @@
 import random
 
 nums =[1,2,3,4,5,6,7,8,9]
-swords =[
+small_words =[
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ]
-cwords =[
+capital_words =[
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 ]
-infu = int(input("Enter a num for pass lenght>>"))
-ln = random.randint(8,infu)
+input_from_user = int(input("Enter a num for pass lenght>>"))
+lenght = random.randint(8,input_from_user)
 pre_password =[]
 
 
 
-for i in range(1,ln+1):
-    intorstr = random.randint(1,2)
+for i in range(1,lenght+1):
+    int_or_str = random.randint(1,2)
 
-    if intorstr ==1:
-        chosenums = random.randint(1,len(nums))
-        pre_password.append(chosenums)
 
-    elif intorstr ==2:
+    if int_or_str ==1:
+        chose_nums = random.randint(1,len(nums))
+        pre_password.append(chose_nums)
 
-        cors =random.randint(1,2)
+    elif int_or_str ==2:
 
-        if cors ==1:
-            incwords = random.randint(1,len(cwords))
-            chosecwords = cwords[incwords]
-            pre_password.append(chosecwords)
+        capital_or_small =random.randint(1,2)
 
-        elif cors ==2:
-            insword = random.randint(1,len(swords))
-            choseswords = swords[insword]
-            pre_password.append(choseswords)
+        if capital_or_small ==1:
+            index_of_capital_words = random.randint(1,len(capital_words))
+            chose_capital_words = capital_words[index_of_capital_words]
+            pre_password.append(chose_capital_words)
+
+        elif capital_or_small ==2:
+            index_of_small_word = random.randint(1,len(small_words))
+            chose_small_words = small_words[index_of_small_word]
+            pre_password.append(chose_small_words)
 
 password =""
 for j in pre_password:
