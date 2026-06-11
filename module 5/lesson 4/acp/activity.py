f1cccc81e41652aa6e4808476b360ca2fe4bad74
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-06-11 19:54:15 +0600

    aaa

---
+++ module 5/lesson 4/acp/activity.py
@@ -0,0 +1,16 @@
+class rev:
+    def __init__(self,s):
+        self.s = s
+
+
+    def reversed(self):
+        st = "".join(reversed(self.s))
+        prev_st = "".join(reversed(st))
+        if self.s == prev_st :
+            return st
+
+        
+
+string = input("Please input a str to reverse the str>>>")
+ob = rev(string)
+print(ob.reversed())
