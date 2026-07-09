161c0aba0fab13e0df345c4d704f7e8426761f51
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-07-09 21:20:17 +0600

    a

---
+++ module 6/lesson 1/activity.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("My first game")
+
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.display.flip()
+
+pygame.quit()
