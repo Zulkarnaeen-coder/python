70600167143f15c9f82a3a37f0d84de80f8ba003
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-07-15 21:45:16 +0600

    a

---
+++ module 6/lesson 2/acp/activity.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((640, 480))
+
+green = (0, 255, 0)
+
+blue = (0, 0, 255)
+
+win.fill((255, 255, 255))
+
+pygame.draw.rect(win, green, pygame.Rect(30, 30, 60, 60))
+
+pygame.draw.rect(win, blue, pygame.Rect(100, 205, 60, 60))
+
+pygame.display.update()
+done = True
+while done:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            done = False
+
+pygame.quit()
