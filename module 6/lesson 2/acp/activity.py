ca4f573f9976b8cd8d6682e0b6225bc1f86ac851
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-07-15 21:49:01 +0600

    a

--- module 6/lesson 2/acp/activity.py
+++ module 6/lesson 2/acp/activity.py
@@ -3,15 +3,13 @@ import pygame
 pygame.init()
 
 win = pygame.display.set_mode((640, 480))
-
+pygame.display.set_caption("My Game")
 green = (0, 255, 0)
-
 blue = (0, 0, 255)
 
 win.fill((255, 255, 255))
 
 pygame.draw.rect(win, green, pygame.Rect(30, 30, 60, 60))
-
 pygame.draw.rect(win, blue, pygame.Rect(100, 205, 60, 60))
 
 pygame.display.update()
