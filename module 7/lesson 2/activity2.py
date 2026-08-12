87886c92c07dbcd2bce40e2d273402558363d25d
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2026-08-12 22:51:01 +0600

    a

--- module 7/lesson 2/activity2.py
+++ module 7/lesson 2/activity2.py
@@ -3,15 +3,14 @@ from tkinter import *
 win = Tk()
 win.title("Login")
 win.geometry("300x200")
+fm= Frame(master=win, height=200, width=360)
+lb1 = Label(master=fm, text="Username", bg = "lightblue", fg = "white")
+lb2 = Label(master=fm, text="Email Id", bg = "lightblue", fg = "white")
+lb3 = Label(master=fm, text="Password", bg = "lightblue", fg = "white")
 
-frame = Frame(master=win, height=200, width=360,bg = "lightblue", fg = "white")
-lb1 = Label(master=frame, text="Username", bg = "lightblue", fg = "white")
-lb2 = Label(master=frame, text="Email Id", bg = "lightblue", fg = "white")
-lb3 = Label(master=frame, text="Password", bg = "lightblue", fg = "white")
-
-name = Entry(master=frame)
-email = Entry(master=frame)
-password = Entry(master=frame, show="*")
+name = Entry(master=fm)
+email = Entry(master=fm)
+password = Entry(master=fm, show="*")
 
 def display():
     nm =name.get()
@@ -20,11 +19,11 @@ def display():
     text_box.insert(END, greet)
     text_box.insert(END, msg)
 
-text_box = Text(master=frame, bg = "grey",fg = "white")
+text_box = Text( bg = "blue",fg = "black")
 
 btn = Button(text = "Login", command = display, bg = "lightblue", fg = "white")
 
-frame.place(x =20,y=0)
+fm.place(x =20,y=0)
 lb1.place(x=20,y=20)
 name.place(x=150,y=20)
 lb2.place(x=20,y=80)
@@ -33,6 +32,6 @@ lb3.place(x = 20,y = 140)
 password.place(x = 150,y = 140)
 btn.place(x = 150,y = 180)
 
-text_box.place(x = 20,y = 220)
+text_box.place(y = 220)
 
 win.mainloop()
